e90df58fb2fa7ddb19e565982dbb56067fc8f56b
bhavanishankar7075 <bhavanimandala2004@gmail.com>
2025-09-25 23:02:25 +0530

    small chnages

--- backend/routes/faq.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getFAQs } = require('../controllers/faqController');
-
-router.get('/', getFAQs);
-
-console.log('FAQ route loaded: GET /');
-
-module.exports = router;
--- backend/routes/newsletter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { subscribeToNewsletter } = require('../controllers/newsletterController');
-
-router.post('/', subscribeToNewsletter); // Changed to match frontend
-
-console.log('Newsletter route loaded: POST /');
-
-module.exports = router;
