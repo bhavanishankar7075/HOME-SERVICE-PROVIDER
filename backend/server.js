84038f0f8ad4e52eebd8396ef89603bb771f1d14
bhavanishankar7075 <bhavanimandala2004@gmail.com>
2025-09-25 22:22:55 +0530

    render url added in server.js

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const server = http.createServer(app);
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:5000','https://home-service-provider-ejso.onrender.com'],
+  origin: ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:5000','https://home-service-provider-backend.onrender.com','https://home-service-provider-frontend.onrender.com','https://home-service-provider-admin.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
@@ -96,7 +96,7 @@ app.use((err, req, res, next) => {
 // Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:5000'],
+    origin: ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:5000','https://home-service-provider-backend.onrender.com','https://home-service-provider-frontend.onrender.com','https://home-service-provider-admin.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   },
